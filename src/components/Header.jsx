b7e2e52704790777167e980e9f38ab14f1c52e18
Joel McCandless <mail@joelmccandless.com>
2025-03-27 19:45:22 -0400

    fix Header import

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
-const header = (
-   <header>
-      <h1>Missile Simulator</h1>
-      <ul>
-         <li>Part 1</li>
-         <li>Part 2</li>
-      </ul>
-   </header>
-);
+function Header() {
+   return (
+      <header>
+         <h1>Missile Simulator</h1>
+         <ul>
+            <li>Part 1</li>
+            <li>Part 2</li>
+         </ul>
+      </header>
+   );
+}
+
+export default Header;
